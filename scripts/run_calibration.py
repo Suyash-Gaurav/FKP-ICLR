d2e190eb100e8f500bb5d297cc0e62b27298f55d
Suyash Gaurav <suyashgauravdas@gmail.com>
2026-06-21 20:30:20 +0900

    path-updated

--- scripts/run_calibration.py
+++ scripts/run_calibration.py
@@ -36,6 +36,11 @@ import logging
 import time
 from pathlib import Path
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
 import torch
 
 from fkp.utils.seeding import seed_everything
